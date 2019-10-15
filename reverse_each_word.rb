acd588b95010dde014524b8370832951af655302
Frank Gerold <franketsgerold@gmail.com>
2019-10-15 00:03:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 def reverse_each_word (sentence)
   wordlist = sentence.split(" ")
-  flipped = []
-  wordlist.each do
-    count = 0 
-    |word|  
-    flipped[count]=word.reverse 
-  end
-  flipped.join(" ")
+  wordlist.co
 end
